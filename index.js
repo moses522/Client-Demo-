d3f751c9313aec78823d3e9096a8e38d43571ff1
Your Name <you@example.com>
2025-12-03 14:27:39 +0200

    fixt bug

--- index.js
+++ index.js
@@ -3,26 +3,19 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import morgan from "morgan";
+import path from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-var Name = "";
-app.use(morgan("combined"));
+express().use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function NameGenerator(req, res, next) {
-  console.log(req.body);
-  Name = req.body["first"] + req.body["pet"];
-  next();
-}
-
-app.use(NameGenerator);
-
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/submit", (req, res) => {
